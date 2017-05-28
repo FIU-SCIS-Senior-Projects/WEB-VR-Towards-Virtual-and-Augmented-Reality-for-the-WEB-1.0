cb7f27151b066baeb14480f14b9b868353d5cc26
Hamilton Chevez <hchev001@fiu.edu>
2017-05-27 22:19:12 -0400

    Made changes to names of scripts and html files
    In order to make Webpack work. Webpack now can bundle
    all scripts into chunks and inject them into the right
    html files

--- app/component.js
+++
@@ -1,7 +0,0 @@
-export default (text = 'Hello World') => {
-  const element = document.createElement('div');
-
-  element.innerHTML = text;
-
-  return element;
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const scriptsPath = "./app/scripts/"
+
+function newPage(name){
+  return new HtmlWebpackPlugin({
+    filename: name + '.html',
+    template: './app/views/' + name + '.html',
+    inject: true,
+    chunks: [name]
+  })
+}
+
 const PATHS = {
-  app: "./app/index.js",
+  index: "./app/scripts/index.js",
+  bst: "./app/scripts/BinarySearchTree.js",
+  breadthFirst: scriptsPath + "BreadthFirst.js",
+  depthFirst: scriptsPath + "DepthFirst.js",
+  dblyLinkedList: scriptsPath + "DoublyLinkedList.js",
+  hashTable: scriptsPath + "HashTable.js",
+  singlyLinkList: scriptsPath + "LinkedList.js",
+  queue: scriptsPath + "Queue.js",
+  quickSort: scriptsPath + "QuickSort.ja",
+  selectionSort: scriptsPath + "SelectionSort.js",
+  stack: scriptsPath + "Stack.js",
+
   build: path.resolve(__dirname, 'build'),
+  source: "./app/views/"
 };
 
 module.exports = {
@@ -11,16 +34,34 @@ module.exports = {
   // convention by default so if a directory contains *index.js*,
   // it resolves to that
   entry: {
-    app: PATHS.app,
+    index: "./app/scripts/index.js",
+    BinarySearchTree: "./app/scripts/BinarySearchTree.js",
+    DoublyLinkList: "./app/scripts/DoublyLinkedList.js",
+    HashTable: "./app/scripts/HashTable.js",
+    Queue: "./app/scripts/Queue.js",
+    QuickSort: "./app/scripts/QuickSort.js",
+    SelectionSort: "./app/scripts/SelectionSort.js",
+    Stack: "./app/scripts/Stack.js",
+    BreadthFirst: "./app/scripts/BreadthFirst.js",
+    DepthFirstSearch: "./app/scripts/DepthFirstSearch.js",
+    SinglyLinkedList: "./app/scripts/SinglyLinkedList.js",
+
   },
   output: {
-    path: PATHS.build,
-    filename: 'bundle.js',
-    publicPath: '/build'
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack demo',
-    }),
+    newPage('index'),
+    newPage('BinarySearchTree'),
+    newPage('DoublyLinkList'),
+    newPage('HashTable'),
+    newPage('Queue'),
+    newPage('QuickSort'),
+    newPage('SelectionSort'),
+    newPage('Stack'),
+    newPage('BreadthFirst'),
+    newPage('SinglyLinkedList'),
+    newPage('DepthFirstSearch'),
   ],
 };
