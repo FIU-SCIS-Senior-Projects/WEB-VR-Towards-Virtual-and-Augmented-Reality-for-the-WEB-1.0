d81828a3f256bd7e93eba39e04e444a5c7bb9a20
Pachev Joseph <pachevjoseph@gmail.com>
2017-05-28 15:49:59 -0400

    webvr-input: Adds usb with libusb and new mouse instantiate based on device

--- webvr-input/src/devices/mod.rs
+++ webvr-input/src/devices/mod.rs
@@ -1,1 +1,15 @@
 pub mod mouse;
+
+use spat_input::InputAdapter;
+use devices::mouse::{Event, MouseResolutions};
+
+pub fn handle_mouse(dev: &mut InputAdapter<mouse::Manipulation,mouse::Input,String,mouse::State>){
+
+    println!("Mouse output: {}",dev.output);
+    dev.manipulation.move_mouse(400,200);
+    dev.update_output();
+    println!("New Mouse output: {}",dev.output);
+    dev.manipulation.move_mouse(1,555);
+    dev.update_output();
+    println!("New Mouse output: {}",dev.output);
+}
--- webvr-input/src/main.rs
+++ webvr-input/src/main.rs
@@ -1,17 +1,44 @@
 #[macro_use]
 extern crate serde_json;
+extern crate libusb;
 
 mod spat_input;
 mod devices;
 
-use devices::mouse::{Event, MouseResolutions};
+use std::thread::spawn; //For threads
+use spat_input::InputAdapter;
 
 fn main() {
 
-    let mut m = spat_input::InputAdapter::new_mouse();
-    println!("Mouse output: {}",m.output);
-    m.manipulation.move_mouse(400,200);
-    m.update_output();
-    println!("New Mouse output: {}",m.output);
+
+    
+    let mut m;
+    //Starting off by using the libusb library to read usb devices
+    let context = libusb::Context::new().unwrap();
+
+    for device in context.devices().unwrap().iter() {
+        //TODO: Figure out a way to match product_id to different products
+        let device_desc = device.device_descriptor().unwrap();
+
+        //Pattern matching to product_id to figure out what type of decice
+        //that We're dealing with. Ideally, we'll have a list of supported
+        //device that we can use. For now, it's focused on a set of 
+        //predetermined evices
+        
+        match device_desc.product_id() {
+            49242 => {
+                println!("It's the mouse");
+                m = InputAdapter::new_mouse();
+
+                //Spawning off a thread to handle the mouse even
+                //TODO: THis might need reractoring later based on device type
+                spawn(move||{
+                    devices::handle_mouse(&mut m);
+                });
+            },
+            _=> {}
+        }
+    }
 
 }
+
