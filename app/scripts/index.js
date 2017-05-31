0bbb660a3c3ab7aac53663a158886187edd96e3c
Hamilton Chevez <hchev001@fiu.edu>
2017-05-31 11:05:33 -0400

    added browserify dependency and used it to load jquery and bootstrap locally

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,10 @@
-import 'jquery'
+//import 'jquery'
+var jQuery = require('jquery');
+window.$ = window.jQuery = jQuery;
 
+require('bootstrap');
+
+jQuery.noConflict(true);
 
 console.log('index page script')
 
