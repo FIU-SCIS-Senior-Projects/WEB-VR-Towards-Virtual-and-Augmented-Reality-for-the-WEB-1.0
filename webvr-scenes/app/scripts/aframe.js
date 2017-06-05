63bce552c6ebba06c23dcd4b2d665071b870c83b
Hamilton Chevez <hchev001@fiu.edu>
2017-06-05 00:20:20 -0400

    added a sky to the scene

--- webvr-scenes/app/scripts/aframe.js
+++ webvr-scenes/app/scripts/aframe.js
@@ -1,5 +1,12 @@
 var AFRAME = require('aframe');
+
+var gradientSky = require('aframe-gradient-sky');
+AFRAME.registerComponent("gradientsky", gradientSky);
+
 var draw = require('aframe-draw-component').component;
 var textwrap = require('aframe-textwrap-component').component;
 AFRAME.registerComponent("draw", draw);
 AFRAME.registerComponent("textwrap", textwrap);
+
+
+
