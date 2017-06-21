4c0a3fdd7d2596ed9ad86f9ecead9517438338ec
Hamilton Chevez <hchev001@fiu.edu>
2017-06-21 12:17:48 -0400

    fixed some jquery and bootstrap conflicts

--- webvr-scenes/app/scripts/index.js
+++ webvr-scenes/app/scripts/index.js
@@ -1,32 +1,32 @@
 var jQuery = require('jquery');
 window.$ = window.jQuery = jQuery;
 
-require('tether');
+global.Tether = require('tether');
 require('bootstrap');
-jQuery.noConflict(true);
+// jQuery.noConflict(true);
 
 //require('jquery.easing')(jQuery);
 
 console.log('index page script');
 
 
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+// //jQuery to collapse the navbar on scroll
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+// //jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {
+//     $(document).on('click', 'a.page-scroll', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
 
