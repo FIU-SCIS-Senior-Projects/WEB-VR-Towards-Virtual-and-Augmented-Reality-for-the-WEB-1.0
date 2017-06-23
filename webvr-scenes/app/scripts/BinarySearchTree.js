5caae31a8d2cbd899b2c4740a87392361c73161b
Hamilton Chevez <hchev001@fiu.edu>
2017-06-23 16:34:12 -0400

    added an event emitting component, changed orientation of data set

--- webvr-scenes/app/scripts/BinarySearchTree.js
+++ webvr-scenes/app/scripts/BinarySearchTree.js
@@ -1,1 +1,13 @@
 console.log("BinarySearchTree script")
+
+
+AFRAME.registerComponent('do-something-once-loaded', {
+  init: function() {
+    console.log('I am ready!');
+  }
+});
+
+var sceneEl = document.querySelector('a-scene');
+
+var entityEl = document.createElement('a-entity');
+sceneEl.appendChild(entityEl);
