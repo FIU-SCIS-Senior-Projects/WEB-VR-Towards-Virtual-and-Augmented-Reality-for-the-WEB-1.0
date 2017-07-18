802fc795da5a16085ddcb2d5683001802244524d
Hamilton Chevez <hchev001@fiu.edu>
2017-07-18 16:02:39 -0400

    fixed merge conflicts of master and develop

---
+++ webvr-scenes/app/scripts/SelectionSort.js
@@ -0,0 +1,1 @@
+console.log('selection sort page script')
---
+++ webvr-scenes/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack');
+
+function newPage(name){
+  return new HtmlWebpackPlugin({
+    filename: name + '.html',
+    template: './app/views/' + name + '.html',
+    inject: true,
+    chunks: [name]
+  })
+}
+
+module.exports = {
+  // Entries have to resolve to files! they rely on Node
+  // convention by default so if a directory contains *index.js*,
+  // it resolves to that
+  entry: {
+    index: "./app/scripts/index.js",
+    BinarySearchTree: "./app/scripts/BinarySearchTree.js",
+    DoublyLinkList: "./app/scripts/DoublyLinkedList.js",
+    HashTable: "./app/scripts/HashTable.js",
+    Queue: "./app/scripts/Queue.js",
+    QuickSort: "./app/scripts/QuickSort.js",
+    SelectionSort: "./app/scripts/SelectionSort.js",
+    Stack: "./app/scripts/Stack.js",
+    BreadthFirst: "./app/scripts/BreadthFirst.js",
+    DepthFirstSearch: "./app/scripts/DepthFirstSearch.js",
+    SinglyLinkedList: "./app/scripts/SinglyLinkedList.js",
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'app/buildScripts'),
+    filename: '[name].js',
+  },
+  plugins: [
+    //newPage('index'),
+    //newPage('BinarySearchTree'),
+    //newPage('DoublyLinkList'),
+    //newPage('HashTable'),
+    //newPage('Queue'),
+    //newPage('QuickSort'),
+    //newPage('SelectionSort'),
+    //newPage('Stack'),
+    //newPage('BreadthFirst'),
+    //newPage('SinglyLinkedList'),
+    //newPage('DepthFirstSearch'),
+    //new ExtractTextPlugin("[name].css"),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+  ],
+
+  module: {
+    loaders: [  
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+      }
+    ],
+
+  }
+};
